463bc44091de090b20423e6dea9d61d1978e17f3
Sandesh Apparala <138796263+sandeshapparala@users.noreply.github.com>
2024-02-10 18:39:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     smokeEffect();
 });
+
+
+function disablePointerEventsOnTouch() {
+    const interactionLayer = document.getElementById('interaction-layer');
+    window.addEventListener('touchstart', function() {
+        interactionLayer.style.pointerEvents = 'auto';
+    });
+}
+
+disablePointerEventsOnTouch();
